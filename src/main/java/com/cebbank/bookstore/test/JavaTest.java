aa54625082b73a6ccd741c7fcb8a035514719f6f
zjh779954 <1353882948@qq.com>
2024-09-17 21:23:39 +0800

    779953 test第0行

--- src/main/java/com/cebbank/bookstore/test/JavaTest.java
+++ src/main/java/com/cebbank/bookstore/test/JavaTest.java
@@ -2,6 +2,7 @@ package com.cebbank.bookstore.test;
 
 public class JavaTest {
     public static void main(String[] args) {
+        System.out.println("第0行");
         System.out.println("第1行");
         System.out.println("第2行");
         System.out.println("第3行");
