1bc2f061524e23f21d428f428520dc3d64411a1c
zjh779954 <1353882948@qq.com>
2024-09-19 23:07:03 +0800

    master test

--- src/main/java/com/cebbank/bookstore/test/JavaTest.java
+++ src/main/java/com/cebbank/bookstore/test/JavaTest.java
@@ -2,13 +2,6 @@ package com.cebbank.bookstore.test;
 
 public class JavaTest {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("c");
-        System.out.println("d");
+
     }
 }
