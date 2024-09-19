42a84c2ec6cee5506ffd4a59fd173651d942229c
zjh779954 <1353882948@qq.com>
2024-09-19 23:35:07 +0800

    develop

--- src/main/java/com/cebbank/bookstore/test/JavaTest.java
+++ src/main/java/com/cebbank/bookstore/test/JavaTest.java
@@ -2,12 +2,9 @@ package com.cebbank.bookstore.test;
 
 public class JavaTest {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("c");
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
+        System.out.println(444);
     }
 }
