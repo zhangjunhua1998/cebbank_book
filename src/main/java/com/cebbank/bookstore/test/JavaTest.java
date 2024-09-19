5b3bbfb1c163c2a04a5d6e681c1d2bdbb57bd132
zjh779954 <1353882948@qq.com>
2024-09-19 22:07:55 +0800

    779953 数字2

--- src/main/java/com/cebbank/bookstore/test/JavaTest.java
+++ src/main/java/com/cebbank/bookstore/test/JavaTest.java
@@ -2,19 +2,6 @@ package com.cebbank.bookstore.test;
 
 public class JavaTest {
     public static void main(String[] args) {
-        System.out.println("第0行");
-        System.out.println("第1行");
-        System.out.println("第2行");
-        System.out.println("第3行");
-        System.out.println("第4行");
-        System.out.println("第5行");
-        System.out.println("test第6行");
-        System.out.println("第8行");
-        System.out.println("第11行");
-        System.out.println("第20行");
 
-        System.out.println(1);
-
-        System.out.println("数字2");
     }
 }
